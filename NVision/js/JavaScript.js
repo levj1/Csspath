5a72be886cb0c60d31df95dc2d17954f05725664
levj1 <levj1@yahoo.com>
2017-10-23 19:58:38 -0400

    Latest nvision site

---
+++ NVision/js/JavaScript.js
@@ -0,0 +1,11 @@
+﻿$(window).on('load resize', function () {
+    $('.fullscreen').height($(window).height()).width($(window).width());
+});
+
+$(window).on('scroll', function () {
+    if ($(window).scrollTop() >= $(window).height()) {
+        $('#navmain').addClass('navbar-fixed-top');
+    } else {
+        $('#navmain').removeClass('navbar-fixed-top');
+    }
+});
